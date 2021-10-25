60636b7eb331972bea0f0be458143992604cf905
Rehab B <79226830+er-otaibi@users.noreply.github.com>
2021-10-25 23:53:46 +0300

    retrive

--- NotesApp/app/src/main/java/com/example/notesapp/DBHelper.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/DBHelper.kt
@@ -1,8 +1,11 @@
 package com.example.notesapp
 
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, "myNotes.db", null, 1) {
@@ -15,10 +18,39 @@ class DBHelper(context: Context) : SQLiteOpenHelper(context, "myNotes.db", null,
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {}
 
-    fun savedat(s1: String): Long {
-
+    fun saveNote(s1: String): Long {
         val cv = ContentValues()
         cv.put("Note", s1)
         return sqLiteDatabase.insert("notes", null, cv)
+    }
+
+    @SuppressLint("Range")
+    fun getNote(note: String): String {
+
+        var c : Cursor = sqLiteDatabase.query("notes" , null , "Note=?" , arrayOf(note) , null , null , null)
+        c.moveToFirst()
+
+       return c.getString(c.getColumnIndex("Note"))
+    }
+
+    @SuppressLint("Range")
+    fun readData() {
+        var selectQuery = "SELECT  * FROM notes"
+        var cursor: Cursor? = null
+        try {
+
+            cursor = sqLiteDatabase.rawQuery(selectQuery, null)
+        } catch (e: SQLiteException) {
+            sqLiteDatabase.execSQL(selectQuery)
+        }
+        var note: String
+        if (cursor != null) {
+            if (cursor.moveToFirst()) {
+                do {
+                   note = cursor.getString(cursor.getColumnIndex("Note"))
+                    MyList.notesList.add(note)
+                } while (cursor.moveToNext())
+            }
+        }
     }
 }
--- NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -7,33 +7,43 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.notesapp.MyList.notesList
+
+object MyList{
+
+     var notesList = arrayListOf<String>()
+}
 
 class MainActivity : AppCompatActivity() {
     lateinit var rvMain: RecyclerView
     lateinit var etnote: EditText
     lateinit var addBtn: Button
-    var notesList = arrayListOf<String>()
-    var s1 =""
+    var note =""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rvMain = findViewById(R.id.rvMain)
-        rvMain.adapter = NoteAdapter(notesList)
-        rvMain.layoutManager = LinearLayoutManager(this)
         etnote = findViewById(R.id.etnote)
         addBtn = findViewById(R.id.addBtn)
 
+        var helper = DBHelper(applicationContext)
+        helper.readData()
+
+        rvMain.adapter = NoteAdapter(notesList)
+        rvMain.layoutManager = LinearLayoutManager(this)
+
+
         addBtn.setOnClickListener {
-            s1 = etnote.text.toString()
+            note = etnote.text.toString()
 
-            var helper = DBHelper(applicationContext)
-            var status = helper.savedat(s1)
-//            notesList.add(s1)
-//            rvMain.adapter!!.notifyDataSetChanged()
+            var status = helper.saveNote(note)
             Toast.makeText(this , "Your note is added successfully $status" , Toast.LENGTH_LONG).show()
+
+            var r = helper.getNote(note)
+            etnote.setText("")
+            notesList.add(r)
+            rvMain.adapter!!.notifyDataSetChanged()
         }
-
-
     }
 }
